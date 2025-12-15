f206b964d89c8b1bcdb7032b6031fab2fc44cb83
Nicolas Silva <nical@fastmail.com>
2025-12-15 11:44:37 +0100

    Bug 1978773 - Enable precise linear gradients (GPU-only). r=#gfx-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D266828

--- gfx/wr/wrench/src/wrench.rs
+++ gfx/wr/wrench/src/wrench.rs
@@ -282,7 +282,7 @@ impl Wrench {
             enable_debugger: true,
             precise_radial_gradients: true,
             precise_conic_gradients: true,
-            precise_linear_gradients: window.is_software(),
+            precise_linear_gradients: true,
             ..Default::default()
         };
 
